2aff3006679cb45963f838ad06d8830bf85eab58
CrisBryann <nguyenphuocthienan2018cm@gmail.com>
2025-04-11 03:48:35 +0700

    hi

--- src/hooks/use-buildings.tsx
+++ src/hooks/use-buildings.tsx
@@ -5,7 +5,7 @@ export const useBuildings = () => {
   return useQuery({
     queryKey: ["buildings"],
     queryFn: async () => {
-      const params = { page: 1, limit: 100 };
+      const params = { page: 1, size: 10000, limit: 1000 };
       const response = await getAllBuildings(params);
       await new Promise((resolve) => setTimeout(resolve, 4000));
       return response.payload.items; // API trả về `data`
